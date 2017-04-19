a38c02096d707ea68b19993c16d3b151322524b9
craftsmon <craftsmon@outlook.com>
2017-04-19 23:08:11 +0300

    Blueprints and db migrations

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_migrate import Migrate
 
 
 # local imports
@@ -21,5 +22,18 @@ def create_app(config_name):
     login_manager.init_app(app)
     login_manager.login_message = "You must login to access this page"
     login_manager.login_view = "auth.login"
+
+    migrate = Migrate(app, db)
+
+    from app import models
+
+    from .admin import admin as admin_blueprint
+    app.register_blueprint(admin_blueprint, url_prefix = '/admin')
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    from .home import home as home_blueprint
+    app.register_blueprint(home_blueprint)
 
     return app
