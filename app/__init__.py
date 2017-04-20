2efe5d2a8edb87e2279bb9175f9649cd01a39f46
craftsmon <craftsmon@outlook.com>
2017-04-20 17:25:47 +0300

    Fixed typos

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
+from flask_bootstrap import Bootstrap
 
 
 # local imports
@@ -17,6 +18,8 @@ def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
+
+    Bootstrap(app)
     db.init_app(app)
 
     login_manager.init_app(app)
@@ -35,5 +38,6 @@ def create_app(config_name):
 
     from .home import home as home_blueprint
     app.register_blueprint(home_blueprint)
+
 
     return app
